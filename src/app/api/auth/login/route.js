59d1664f22682ae94da277f428b0654c79f9ac4e
Gourinandhana E S <gourinandhana028@gmail.com>
2026-02-09 20:38:19 +0530

    login credentials chnaged

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -5,7 +5,7 @@ export async function POST(request) {
     const body = await request.json();
     const { username, password } = body;
 
-    if (username === 'gouri' && password === 'gouri@123') {
+    if (username === 'admin' && password === 'admin@123') {
         const cookieStore = await cookies();
         cookieStore.set('auth', 'true', { path: '/', httpOnly: true });
         return NextResponse.json({ success: true });
